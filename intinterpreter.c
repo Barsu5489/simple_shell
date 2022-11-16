06f63776c1f25b0d9c1950dc897fb5910f79d123
Malik <bamjnr99@gmail.com>
2022-11-15 19:38:32 -0800

    command line interpreter.

--- intinterpreter.c
+++ intinterpreter.c
@@ -1,87 +1,41 @@
 #include "main.h"
-
 /**
-
  * main - runs a simple shell
-
  * Return: returns 0 on success, -1 on failure
-
  */
-
 int main(void)
-
 {
-
-	char *buffer = NULL;
-
+	char *buffer = NULL, *str, *av[2];
 	size_t bufsize = 0;
-
 	int i = 0;
-
-	char *str;
-
 	pid_t pid;
 
-	char *av[2];
-
-
-
 	while (1)
-
 	{
-
 		printf("$ ");
-
 		i = getline(&buffer, &bufsize, stdin);
 
 		if (i == -1 || strcmp(buffer, "exit\n") == 0)
-
 		{
-
 			printf("\n");
-
 			break;
-
 		}
-
 		str = strndup(buffer, (i - 1));
-
 		av[0] = str;
-
 		av[1] = NULL;
-
 		pid = fork();
 
 		if (pid == 0)
-
 		{
-
 			if (execve(av[0], av, environ) == -1)
-
-			{
-
-				perror("./shell");
-
+			{	perror("./shell");
 				exit(-1);
-
 			}
-
 		}
-
 		else
-
-		{
-
 			wait(NULL);
-
-		}
-
 	}
-
 	free(str);
-
 	free(buffer);
-
 	return (0);
-
 }
